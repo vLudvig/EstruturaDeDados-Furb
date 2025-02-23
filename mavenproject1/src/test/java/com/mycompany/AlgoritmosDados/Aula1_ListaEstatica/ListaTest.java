f639677395d0835b8440697caf8f8eb48ce1ee09
vLudvig <ludvig.vitor@gmail.com>
2025-02-23 15:46:09 -0300

    Finalizado ListaEstatica e Testes

---
+++ mavenproject1/src/test/java/com/mycompany/AlgoritmosDados/Aula1_ListaEstatica/ListaTest.java
@@ -0,0 +1,194 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.AlgoritmosDados.Aula1_ListaEstatica;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author User
+ */
+public class ListaTest {
+    
+    public ListaTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Testar método de inclusão de dados na lista
+     */
+    @Test
+    public void testInserir() {
+        System.out.println("inserir");
+        int novoNumero = 5;
+        Lista instance = new Lista();
+        instance.inserir(novoNumero);
+        novoNumero = 10;
+        instance.inserir(novoNumero);
+        novoNumero = 15;
+        instance.inserir(novoNumero);
+        novoNumero = 20;
+        instance.inserir(novoNumero);
+        
+        assertEquals("5,10,15,20", instance.toString());
+    }
+
+    /**
+     * Testar método de obtenção de tamanho da lista
+     */
+    @Test
+    public void testGetTamanho() {
+        System.out.println("Obtenção de tamanho da lista");
+        int novoNumero = 5;
+        Lista instance = new Lista();
+        instance.inserir(novoNumero);
+        novoNumero = 10;
+        instance.inserir(novoNumero);
+        novoNumero = 15;
+        instance.inserir(novoNumero);
+        novoNumero = 20;
+        instance.inserir(novoNumero);
+        
+        assertEquals(4, instance.getTamanho());
+    }
+
+    /**
+     * Testar método buscar() com elemento existente
+     */
+    @Test
+    public void testBuscar() {
+        System.out.println("buscar");
+        int novoNumero = 5;
+        Lista instance = new Lista();
+        instance.inserir(novoNumero);
+        novoNumero = 10;
+        instance.inserir(novoNumero);
+        novoNumero = 15;
+        instance.inserir(novoNumero);
+        novoNumero = 20;
+        instance.inserir(novoNumero);
+        
+        assertEquals(2, instance.buscar(15));
+    }
+
+    /**
+     * Testar método buscar() com elemento inexistente
+     */
+    @Test
+    public void testBuscarInexist() {
+        System.out.println("buscar inexistente");
+        int novoNumero = 5;
+        Lista instance = new Lista();
+        instance.inserir(novoNumero);
+        novoNumero = 10;
+        instance.inserir(novoNumero);
+        novoNumero = 15;
+        instance.inserir(novoNumero);
+        novoNumero = 20;
+        instance.inserir(novoNumero);
+        
+        assertEquals(-1, instance.buscar(30));
+    }
+
+    /**
+     * Testar método retirar()
+     */
+    @Test
+    public void testRetirar() {
+        System.out.println("Retirar");
+        int novoNumero = 5;
+        Lista instance = new Lista();
+        instance.inserir(novoNumero);
+        novoNumero = 10;
+        instance.inserir(novoNumero);
+        novoNumero = 15;
+        instance.inserir(novoNumero);
+        novoNumero = 20;
+        instance.inserir(novoNumero);
+        
+        instance.retirar(10);
+        
+        assertEquals("5,15,20", instance.toString());
+        assertEquals(3, instance.getTamanho());
+    }
+
+    /**
+     * Testar inclusão que provoque redimensionamento
+     */
+    @Test
+    public void testRedimensionamento() {
+        System.out.println("Redimensionamento");
+        Lista instance = new Lista();
+        for(int i = 0; i < 15; i++){
+            instance.inserir(i + 1);
+        }
+        
+        String expResultString = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15";
+        int expResultTamanho = 15;
+        
+        assertEquals(expResultString, instance.toString());
+        assertEquals(expResultTamanho, instance.getTamanho());
+    }
+
+    /**
+     * Obter Elemento.
+     */
+    @Test
+    public void testObterElemento() {
+        System.out.println("ObterElemento");
+        int novoNumero = 5;
+        Lista instance = new Lista();
+        instance.inserir(novoNumero);
+        novoNumero = 10;
+        instance.inserir(novoNumero);
+        novoNumero = 15;
+        instance.inserir(novoNumero);
+        novoNumero = 20;
+        instance.inserir(novoNumero);
+        
+        assertEquals(20, instance.obterElemento(3));
+    }
+
+    /**
+     * Liberar
+     */
+    @Test
+    public void testLiberar() {
+        System.out.println("toString");
+        int novoNumero = 5;
+        Lista instance = new Lista();
+        instance.inserir(novoNumero);
+        novoNumero = 10;
+        instance.inserir(novoNumero);
+        novoNumero = 15;
+        instance.inserir(novoNumero);
+        novoNumero = 20;
+        instance.inserir(novoNumero);
+        
+        instance.liberar();
+        
+        assertEquals(true, instance.estaVazia());
+    }
+}
